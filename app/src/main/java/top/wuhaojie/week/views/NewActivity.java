07991073e3d3830c47088cda03da4b291976b982
wuhaojie <w19961009@126.com>
2016-12-05 13:27:17 +0800

    add new act ui.

--- app/src/main/java/top/wuhaojie/week/views/NewActivity.java
+++ app/src/main/java/top/wuhaojie/week/views/NewActivity.java
@@ -5,6 +5,8 @@ import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.EditText;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -15,9 +17,13 @@ public class NewActivity extends AppCompatActivity {
     @BindView(R.id.toolbar)
     Toolbar mToolbar;
     @BindView(R.id.fab)
-    FloatingActionButton Fab;
+    FloatingActionButton mFab;
     @BindView(R.id.cl)
     CoordinatorLayout mCl;
+    @BindView(R.id.et_title)
+    EditText mEtTitle;
+    @BindView(R.id.et_content)
+    EditText mEtContent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +31,15 @@ public class NewActivity extends AppCompatActivity {
         setContentView(R.layout.activity_new);
         ButterKnife.bind(this);
         setSupportActionBar(mToolbar);
+        if (getSupportActionBar() != null)
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        mEtContent.requestFocus();
     }
 
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) finish();
+        return super.onOptionsItemSelected(item);
+    }
 }

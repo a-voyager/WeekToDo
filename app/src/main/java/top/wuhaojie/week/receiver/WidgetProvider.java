eab56769f426702ee0de89cf1d98a337dd090452
wuhaojie <w19961009@126.com>
2017-02-14 22:41:18 +0800

    add widget onclick. test not pass.

--- app/src/main/java/top/wuhaojie/week/receiver/WidgetProvider.java
+++ app/src/main/java/top/wuhaojie/week/receiver/WidgetProvider.java
@@ -1,9 +1,16 @@
 package top.wuhaojie.week.receiver;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
+import android.widget.RemoteViews;
+
+import top.wuhaojie.week.R;
+import top.wuhaojie.week.constant.Constants;
+import top.wuhaojie.week.views.NewActivity;
 
 /**
  * Author: wuhaojie
@@ -14,10 +21,23 @@ import android.content.Intent;
 
 public class WidgetProvider extends AppWidgetProvider {
 
+    private static final String TAG = "WidgetProvider";
+
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        super.onUpdate(context, appWidgetManager, appWidgetIds);
-        // TODO: 17-2-13 set Onclick Listener
+        for (int id : appWidgetIds) {
+            Intent intent = new Intent(context, NewActivity.class);
+            intent.putExtra(Constants.INTENT_EXTRA_MODE_OF_NEW_ACT, Constants.MODE_OF_NEW_ACT.MODE_QUICK);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+            String packageName = context.getPackageName();
+            RemoteViews rv = new RemoteViews(packageName, R.layout.widget_new_task);
+            rv.setOnClickPendingIntent(R.id.ll_widget, pendingIntent);
+
+            appWidgetManager.updateAppWidget(id, rv);
+
+        }
+
     }
 
     @Override
@@ -26,4 +46,9 @@ public class WidgetProvider extends AppWidgetProvider {
     }
 
 
+    @Override
+    public void onEnabled(Context context) {
+        super.onEnabled(context);
+        Log.d(TAG, "onEnabled: ");
+    }
 }
